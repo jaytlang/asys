df4cf448f05e383565b110a18b23359002c38695
Jay Lang <jaytlang@mit.edu>
2021-01-26 10:00:52 -0500

    Nits: make ps actually run
    I had used ps.c as a scratch template and didn't realize it wouldn't
    compile at the moment. Fix that.
    
    Signed-off-by: Jay Lang <jaytlang@mit.edu>

--- src/ps.c
+++ src/ps.c
@@ -1,14 +1,10 @@
 /* Process server */
 
-struct psproc{
-	unsigned long pid;
-	char *name;
+__attribute__((section(".text.ps"))) void
+psmain(void)
+{
+	int i;
 
-	__attribute__((section(".text.ps"))) void
-	psmain(void)
-	{
-		int i;
-
-		i = 0;
-		for(;;) i++;
-	}
+	i = 0;
+	for(;;) i++;
+}
